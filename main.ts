ee0700311880ec18269264dedb623baa6aefff8a
leye20 <leyeagbo@gmail.com>
2020-04-03 05:59:11 -0600

    second commit

--- main.ts
+++ main.ts
@@ -18,5 +18,15 @@ console.log(addUp(4));
  * Create a function that takes a string and returns a string in which each character is repeated once.
  */
 
+function doubleChar(str: String) {
+    let arr = str.split('');
+    let result = [];
 
+    for (let i = 0; i < arr.length; i++) {
+        result.push(arr[i], arr[i])
+    }
+    return result.join('')
+}
+
+console.log(doubleChar('well it worked'));
  
